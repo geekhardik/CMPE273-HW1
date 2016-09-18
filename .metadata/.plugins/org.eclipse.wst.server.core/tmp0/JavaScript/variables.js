4144b83b360dfb877fb1748cf39b8155549ecf3f
geekhardik <hardik.p.shah@sjsu.edu>
2016-09-17 21:59:38 -0700

    added .metadata

---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/JavaScript/variables.js
@@ -0,0 +1,28 @@
+var name = "Hardik Shah";	//global variable
+	//document.getElementById('global').innerHTML = "global varliable value : " + name;
+console.log(name);	
+//
+//	 function myfunction(){
+//		
+//		//local variables!
+//		var name1 = "Engineer Hardik";	
+//		var _123 = 123;
+//		var check;
+//		
+//		document.getElementById('local').innerHTML = "local varliable value : " +name1;
+//		document.getElementById('incorrect').innerHTML = "'_123' varliable value : " + _123;
+//		document.getElementById('undefined').innerHTML = "'check' varliable value : " + check;		
+//	} 
+//	 
+//	 function convert(){
+//		 var h = Number(document.getElementById('height').value);
+//		 if(isNaN(h)|| h == null || h == ""){
+//			 alert("incorrect input!");
+//			 location.reload();
+//			 return false;
+//		 }else{
+//			 var result;
+//			 result = h*12;
+//			 document.getElementById('result').innerHTML = "Converted height in inches is :  " + result;
+//		 }
+//	 }
